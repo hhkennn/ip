2990a06af109bca93318511b76eabe14da931a47
Hoong Ken <hhken1129@gmail.com>
2026-08-17 21:11:42 +0800

    Add command echo and exit behavior

--- src/main/java/Herta.java
+++ src/main/java/Herta.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
+
 public class Herta {
+    private static final String INDENT = "     ";
+
+    /**
+     * Prints each line of chatbot output with the standard indentation.
+     *
+     * @param message the message to print
+     */
+    private static void printIndented(String message) {
+        // Split message wherever a line break occurs
+        String[] lines = message.split("\\R");
+
+        for (int i = 0; i < lines.length; i++) {
+            System.out.println(INDENT + lines[i]);
+        }
+    }
+
     public static void main(String[] args) {
         String banner = " _   _           _        \n"
                 + "| | | | ___ _ __| |_ __ _\n"
@@ -7,12 +25,26 @@ public class Herta {
                 + "|_| |_|\\___|_|   \\__\\__,_|\n";
         String separator = "____________________________________________________________";
 
-        System.out.println(separator);
-        System.out.print(banner);
-        System.out.println("Hello! I'm Herta.");
-        System.out.println("What can I do for you?");
-        System.out.println(separator);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(separator);
+        printIndented(separator);
+        printIndented(banner);
+        printIndented("Hello! I'm Herta.");
+        printIndented("What can I do for you?");
+        printIndented(separator);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            printIndented(separator);
+
+            if (input.equals("bye")) {
+                printIndented("Bye. Hope to see you again soon!");
+                printIndented(separator);
+                break;
+            }
+
+            printIndented(input);
+            printIndented(separator);
+        }
     }
 }
