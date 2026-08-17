2e86f031b3522d27d738460317ab9f537ea52295
Hoong Ken <hhken1129@gmail.com>
2026-08-17 17:17:57 +0800

    Rename chatbot to Herta

---
+++ src/main/java/Herta.java
@@ -0,0 +1,10 @@
+public class Herta {
+    public static void main(String[] args) {
+        String banner = " _   _           _        \n"
+                + "| | | | ___ _ __| |_ __ _\n"
+                + "| |_| |/ _ \\ '__| __/ _` |\n"
+                + "|  _  |  __/ |  | || (_| |\n"
+                + "|_| |_|\\___|_|   \\__\\__,_|\n";
+        System.out.println(banner);
+    }
+}
