3c0856eaef7e42e3091bd1de60231cb848d266da
Hoong Ken <hhken1129@gmail.com>
2026-08-17 17:29:37 +0800

    Add greeting and exit messages

--- src/main/java/Herta.java
+++ src/main/java/Herta.java
@@ -5,6 +5,14 @@ public class Herta {
                 + "| |_| |/ _ \\ '__| __/ _` |\n"
                 + "|  _  |  __/ |  | || (_| |\n"
                 + "|_| |_|\\___|_|   \\__\\__,_|\n";
-        System.out.println(banner);
+        String separator = "____________________________________________________________";
+
+        System.out.println(separator);
+        System.out.print(banner);
+        System.out.println("Hello! I'm Herta.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
